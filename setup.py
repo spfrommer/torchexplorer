802b610a226410de0fb67db9c3309613e7afe290
Samuel Pfrommer <sam.pfrommer@berkeley.edu>
2023-11-17 15:57:32 -0800

    Fix setup.py to use 0.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='torchexplorer',
-    version='0.2.2',
+    version='0.2.3',
     description="Interactively inspect pytorch modules during training.",
     long_description=long_description,
     long_description_content_type="text/markdown",
