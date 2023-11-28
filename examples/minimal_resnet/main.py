9f8602c778f40d3f06678e007f932e418d3dd0c8
Samuel Pfrommer <sam.pfrommer@berkeley.edu>
2023-11-28 10:17:05 -0800

    Refactor examples and add minimal resnet example

---
+++ examples/minimal_resnet/main.py
@@ -0,0 +1,14 @@
+import torch
+import torchvision
+from torchexplorer import watch
+
+model = torchvision.models.resnet18(pretrained=False)
+dummy_X = torch.randn(5, 3, 32, 32)
+
+# Only log input/output histograms, if you don't want even these set log=[].
+watch(model, log_freq=1, log=['io'], backend='standalone')
+
+# Do one forwards and backwards pass
+model(dummy_X).sum().backward()
+
+# Your model will be available at http://localhost:5000
