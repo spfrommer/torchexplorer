bc08f18c237b99f5d929e05d1a06e1cd5e1dffd9
Samuel Pfrommer <sam.pfrommer@berkeley.edu>
2023-12-10 20:45:17 -0800

    Tweak README and example comment

--- examples/minimal_resnet/main.py
+++ examples/minimal_resnet/main.py
@@ -5,7 +5,7 @@ from torchexplorer import watch
 model = torchvision.models.resnet18(pretrained=False)
 dummy_X = torch.randn(5, 3, 32, 32)
 
-# Only log input/output histograms, if you don't want even these set log=[].
+# Only log input/output and parameter histograms, if you don't want even these set log=[].
 watch(model, log_freq=1, log=['io', 'params'], backend='standalone')
 
 # Do one forwards and backwards pass
